e84a7a0feb4b0550c2e671f314f4787640b96f1c
lixuancong <xuancong.li@ubtrobot.com>
2025-11-12 10:03:43 +0800

    update path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python  
-# _*_ coding:utf-8 _*_  
-#  
-# @Version : 1.0  
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+#
+# @Version : 1.0
 # @Time    : 2019/11/1
 # @Author  : 圈圈烃
 # @File    : main
@@ -13,16 +13,28 @@ import Scel2Txt
 import os
 
 # 下载类别
-Categories = ['城市信息:167', '自然科学:1', '社会科学:76', '工程应用:96', '农林渔畜:127', '医学医药:132',
-              '电子游戏:436', '艺术设计:154', '生活百科:389', '运动休闲:367', '人文科学:31', '娱乐休闲:403']
+Categories = [
+    # '城市信息:360',
+    '自然科学:1',
+    '社会科学:76',
+    '工程应用:96',
+    '农林渔畜:127',
+    '医学医药:132',
+    '电子游戏:436',
+    '艺术设计:154',
+    '生活百科:389',
+    '运动休闲:367',
+    '人文科学:31',
+    '娱乐休闲:403'
+]
 # Scel保存路径
-SavePath = r"f:\Users\QQT\Documents\Temp\scel1"
+SavePath = r"/home/xuancong/sogou/ciku"
 
 # TXT保存路径
-txtSavePath = r"f:\Users\QQT\Documents\Temp\txt1"
+txtSavePath = r"/home/xuancong/sogou/txt"
 
 # 开始链接
-startUrl = "https://pinyin.sogou.com/dict/cate/index/436"
+startUrl = "https://pinyin.sogou.com/dict/cate/index/1"
 
 
 def main():
@@ -36,7 +48,8 @@ def main():
     # 我需要啥
     myCategoryUrls = []
     for mc in Categories:
-        myCategoryUrls.append("https://pinyin.sogou.com/dict/cate/index/" + mc.split(":")[-1])
+        myCategoryUrls.append(
+            "https://pinyin.sogou.com/dict/cate/index/" + mc.split(":")[-1])
     print(myCategoryUrls)
     # 大类分类
     for index, categoryOneUrl in enumerate(myCategoryUrls):
